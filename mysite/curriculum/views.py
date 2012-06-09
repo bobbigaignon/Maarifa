47f238ebed516ecb25ea8595bac6fc8459d8228f
Robert Bigaignon <bob.bigaignon@gmail.com>
2012-06-09 13:53:58 -0700

    Add user prototype interface

--- mysite/curriculum/views.py
+++ mysite/curriculum/views.py
@@ -1,1 +1,6 @@
-# Create your views here.
+from django.template import Context, loader
+from django.shortcuts import render_to_response
+
+def index(request):
+    loader.get_template('curriculum/interface.html')
+    return render_to_response('curriculum/interface.html', {})
