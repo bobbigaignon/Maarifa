c0016bb96408870763800837b304232bf816794b
Robert Bigaignon <bob.bigaignon@gmail.com>
2012-06-09 23:27:26 -0700

    add support for TAKE

--- mysite/curriculum/query_processor.py
+++ mysite/curriculum/query_processor.py
@@ -1,17 +1,57 @@
 import re
+from random import choice
 
 from curriculum.models import Curriculum
 from curriculum.models import Unit
+from curriculum.models import Quizz
+from curriculum.models import Question
+from curriculum.models import Choice
 
 
 class QueryProcesser(object):
     flags = re.IGNORECASE
     get_matcher = re.compile('GET *(?P<curriculum>\w+) *(?P<level>\d+)$', flags=flags)
+    take_matcher = re.compile('TAKE *(?P<curriculum>\w+) *(?P<level>\d+)$', flags=flags)
+    submit_matcher = re.compile('SUBMIT *(?P<quizz_hash>\w+) *(?P<answers>.+)$', flags=flags)
 
     def _get_curriculum(self, curriculum_code, level):
         curriculum = Curriculum.objects.get(code=curriculum_code)
         unit = Unit.objects.get(curriculum=curriculum, level=level)
         return unit.body
+
+    def _format_quizz(self, quizz):
+        formatted_questions = []
+        questions = Question.objects.filter(quizz=quizz).all()
+        question_number = 1
+        for q in questions:
+            formatted_question = ["{0}- {1}".format(question_number, q.body)]
+
+            # Load possible choices
+            formatted_choices = []
+            choice_number = 1
+            choices = Choice.objects.filter(question=q).all()
+            for c in choices:
+                formatted_choice = "{0}) {1}".format(
+                    chr(65 + choice_number - 1),
+                    c.body,
+                    )
+                formatted_choices.append(formatted_choice)
+                choice_number = choice_number + 1
+
+            formatted_question.extend(formatted_choices)
+            formatted_questions.append(
+                '\n'.join(formatted_question),
+                )
+
+            question_number = question_number + 1
+
+        return '\n\n'.join(formatted_questions)
+
+    def _get_quizz(self, curriculum_code, level):
+        curriculum = Curriculum.objects.get(code=curriculum_code)
+        unit = Unit.objects.get(curriculum=curriculum, level=level)
+        quizzes = Quizz.objects.filter(unit=unit).all()
+        return self._format_quizz(choice(quizzes))
 
     def _process_user_request(self, request_string):
         """Analyze a user string and response accordingly.
@@ -42,6 +82,13 @@ class QueryProcesser(object):
         if self.get_matcher.match(request_string) is not None:
             matcher = self.get_matcher.match(request_string)
             return self._get_curriculum(
+                matcher.group('curriculum'),
+                matcher.group('level'),
+                )
+
+        elif self.take_matcher.match(request_string) is not None:
+            matcher = self.take_matcher.match(request_string)
+            return self._get_quizz(
                 matcher.group('curriculum'),
                 matcher.group('level'),
                 )
